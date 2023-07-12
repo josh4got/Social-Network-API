1230eaa8f0d47bf8bd35492175be90ef15ba3602
josh4got <125082025+josh4got@users.noreply.github.com>
2023-07-12 08:13:40 -0600

    ROUTES:User Routes
    -Created route to get all users
    -Created route to get one user by id
    -Created route to update user by id
    -Created route to delete user by id and delete associated user thoughts

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -4,11 +4,71 @@ const User = require('../../models/User');
 const Thought = require('../../models/Thought');
 
 // GET all users
+router.get('/users', (req, res) => {
+    User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+});
 // GET a single user by its `_id` and populated thought and friend data
+router.get('/users/:id', (req, res) => {
+    User.findOne({ _id: req.params.id })
+        .populate('thoughts')
+        .populate('friends')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+});
 // POST a new user:
+router.post('/users', (req, res) => {
+    User.create(req.body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+});
 // Put to update a user by its `_id`
+router.put('/users/:id', ({ params, body }, res) => {
+    const { id } = params;
+    User.findOneAndUpdate({ _id: id }, body, { new: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                console.log('No user found with this id!');
+                res.sendStatus(404);
+            } else {
+                res.json(dbUserData);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+});
 // DELETE to remove user by its `_id`
-// BONUS: Remove a user's associated thoughts when deleted.
+router.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    User.findOneAndDelete({ _id: id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                console.log('No user found with this id!');
+                res.sendStatus(404);
+            }
+            // Remove a user's associated thoughts when deleted.
+            return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+        })
+        .then(() => {
+            res.json({ message: 'User and associated thoughts deleted!' });
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+});
 
 // /api/users/:userId/friends/:friendId
 

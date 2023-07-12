62794e2f8e10baed83d056e4a7191eb023e06210
josh4got <125082025+josh4got@users.noreply.github.com>
2023-07-11 20:58:55 -0600

    ROUTES:Thougts Routes
    -Created route to create a reactions to a thought by its id
    -Created route to delete a reaction to a thought by its id
    -Imported dependencies and Exported routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,7 +1,10 @@
-// api/thoughts
+const express = require('express');
+const router = express.Router();
+const Thought = require('../../models/Thought');
+const User = require('../../models/User');
 
 // GET to get all thoughts
-app.get('/thoughts', (req, res) => {
+router.get('/thoughts', (req, res) => {
     Thought.find({})
         .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => {
@@ -9,8 +12,9 @@ app.get('/thoughts', (req, res) => {
             res.sendStatus(400);
         });
 });
+
 // GET to get a single thought by its _id
-app.get('/thoughts/:id', (req, res) => {
+router.get('/thoughts/:id', (req, res) => {
     Thought.findOne({ _id: req.params.id })
         .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => {
@@ -18,8 +22,9 @@ app.get('/thoughts/:id', (req, res) => {
             res.sendStatus(400);
         });
 });
+
 // POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-app.post('/thoughts', (req, res) => {
+router.post('/thoughts', (req, res) => {
     Thought.create(req.body)
         .then(dbThoughtData => {
             return User.findOneAndUpdate(
@@ -28,30 +33,33 @@ app.post('/thoughts', (req, res) => {
                 { new: true}
             );
         })
-        .then(() => res.json(dbThoughtData))
+        .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => {
             console.log(err);
             res.sendStatus(400);
         });
 });
+
 // PUT to update a thought by its '_id'
-app.put('/thoughts/:id', ({ params, body }, res) => {
+router.put('/thoughts/:id', ({ params, body }, res) => {
     const { id } = params;
-    Thought.findOneAndUpdate(id, body, { new: true })
+    Thought.findOneAndUpdate({ _id: id }, body, { new: true })
         .then(dbThoughtData => {
             if (!dbThoughtData) {
                 console.log('No thought found with this id!');
                 res.sendStatus(404);
+            } else {
+                res.json(dbThoughtData);
             }
-            res.json(dbThoughtData);
         })
         .catch(err => {
             console.log(err);
             res.sendStatus(400);
         });
 });
+
 // DELETE to remove a thought by its '_id'
-app.delete('/thoughts/:id', (req, res) => {
+router.delete('/thoughts/:id', (req, res) => {
     const { id: thoughtId } = req.params;
     Thought.findOneAndDelete({ _id: thoughtId })
       .then(dbThoughtData => {
@@ -70,8 +78,52 @@ app.delete('/thoughts/:id', (req, res) => {
         console.log(err);
         res.sendStatus(400);
       });
-  });
-// /api/thoughts/:thoughtId/reactions
+});
 
 // POST to create a reaction stored in a single thought's reactions array field
+router.post('/thoughts/:thoughtId/reactions', (req, res) => {
+    const { thoughtId } = req.params;
+    Thought.findOneAndUpdate(
+      { _id: thoughtId },
+      { $push: { reactions: req.body } },
+      { new: true }
+    )
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          console.log('No thought found with this id!');
+          res.sendStatus(404);
+        } else {
+          res.json(dbThoughtData);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+});
+
 // DELETE to pull and remove a reaction by the reaction's reactionId value
+router.delete('/thoughts/:thoughtId/reactions/:reactionId', (req, res) => {
+    const { thoughtId, reactionId } = req.params;
+    Thought.findOneAndUpdate(
+      { _id: thoughtId },
+      { $pull: { reactions: { reactionId } } },
+      { new: true }
+    )
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          console.log('No thought found with this id!');
+          return res.sendStatus(404);
+        }
+        if (dbThoughtData.reactions.length === 0) {
+          console.log('No reaction found with this id!');
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+});
+
+module.exports = router;
